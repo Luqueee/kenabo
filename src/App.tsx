f2ff44edae3959362d47443c741e66adbd1e8250
luqueee <luqueee_@outlook.es>
2026-05-01 21:59:14 +0200

    files

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { Button } from "@/components/ui/button"
+import type { CSSProperties } from "react"
+import { AppSidebar } from "@/components/app-sidebar"
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+import { TooltipProvider } from "@/components/ui/tooltip"
 
-export function App() {
+export default function App() {
   return (
-    <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
-    </div>
+    <TooltipProvider>
+      <SidebarProvider
+        style={
+          {
+            "--sidebar-width": "calc(var(--spacing) * 72)",
+            "--header-height": "calc(var(--spacing) * 12)",
+          } as CSSProperties
+        }
+      >
+        <AppSidebar variant="inset" />
+        <SidebarInset>
+          <p>aa</p>
+        </SidebarInset>
+      </SidebarProvider>
+    </TooltipProvider>
   )
 }
-
-export default App
---
+++ src/components/app-sidebar.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+import { Sidebar } from "@/components/ui/sidebar"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return <Sidebar collapsible="offcanvas" {...props}></Sidebar>
+}
